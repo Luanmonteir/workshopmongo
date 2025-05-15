88a60e402c8a963fc48d8651b2e82c1f813762b1
Luanmonteir <luandasnevesmonteirocardoso@gmail.com>
2025-05-15 15:24:56 -0300

    Consulta simples com @Query

--- src/main/java/com/luanmonteiro/workshopmongo/post/repository/PostRepository.java
+++ src/main/java/com/luanmonteiro/workshopmongo/post/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.luanmonteiro.workshopmongo.post.repository;
 
 import com.luanmonteiro.workshopmongo.post.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
-
-
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
